f9fc66ef00d0bdddb18a4de7bb8ad52e5d29f809
Luyee Wong <111058709+wongluyee@users.noreply.github.com>
2022-12-28 09:26:29 +0900

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+  let newString = '';
+  if (num < 0 || num !== NaN) {
+    return "ERROR"; 
+  } else {
+    for (i = 0; i < num; i++){
+      newString += string;
+    }
+    return newString;
+  }
 };
 
 // Do not edit below this line
