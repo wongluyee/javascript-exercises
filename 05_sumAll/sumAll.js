709bc1067ddb939ab569b812f4a925b899f498e6
Luyee Wong <111058709+wongluyee@users.noreply.github.com>
2022-12-28 12:36:50 +0900

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+  if (a < 0 || b < 0) return "ERROR";
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  
+  let sum = 0;
+  if (a < b) {
+    for (i = a; i <= b; i++) {
+      sum += i;
+    }
+  } else if (a > b) {
+    for (i = b; i <= a; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
