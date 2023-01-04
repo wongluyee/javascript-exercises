96e67b99580765665bc6b6fe42cfb19f7055d193
Luyee Wong <111058709+wongluyee@users.noreply.github.com>
2023-01-04 21:45:48 +0900

    Done calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, currentValue) => total + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentValue) => total * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base,exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) return 1;
+  
+  let final = 1;
+  for (i = n; i > 0; i--){
+      final *= i;
+  }
+  return final;
 };
 
 // Do not edit below this line
