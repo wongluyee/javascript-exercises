39d054abe40ac524c7ad1a8fc72b696f148bd687
Luyee Wong <111058709+wongluyee@users.noreply.github.com>
2023-01-05 22:33:48 +0900

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let modifiedString = str.toLowerCase().replace(/[^a-z]/g, '');
+    let reverseString = modifiedString.split('').reverse().join('');
+    return reverseString === modifiedString;
 };
 
 // Do not edit below this line
