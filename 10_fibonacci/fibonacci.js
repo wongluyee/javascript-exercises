5681b75f098120886b515111c405b03a32d02c1b
Luyee Wong <111058709+wongluyee@users.noreply.github.com>
2023-01-07 16:36:14 +0900

    Update fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    //When a string such as "10" is entered, c = n*1 will convert it into number.
+    let a = 0, b = 1, c = n * 1;
 
+    if (n < 0) return "OOPS";
+  
+    for(let i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
