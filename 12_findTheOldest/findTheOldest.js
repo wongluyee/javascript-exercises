66b6a4a765b34955c650ef62fbaa1dbbd62dd6be
Luyee Wong <111058709+wongluyee@users.noreply.github.com>
2023-01-07 17:39:45 +0900

    Update findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    //Find the age first
+    const age = function(birth,death) {
+        //If no yearOfDeath is given, use this year.
+        if (!death){
+            death = new Date().getFullYear();
+        } 
+        return death - birth;
+    }
 
+    return array.reduce((oldest, currentPerson) => {
+        //Find the older age
+        const oldestAge = age(oldest.yearOfBirth,oldest.yearOfDeath);
+      
+        //Find the younger age
+        const currentAge = age(currentPerson.yearOfBirth,currentPerson.yearOfDeath);
+        
+        //If oldestAge is higher than currentAge, return the older people
+        return oldestAge > currentAge? oldest:currentPerson;
+    })
 };
 
 // Do not edit below this line
